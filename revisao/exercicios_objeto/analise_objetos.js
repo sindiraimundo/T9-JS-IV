511d4efa85f0ea183d47f8af117a4a1fa1d77e38
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-06-02 12:12:07 -0300

    Resolução de exercício de analise de objeto

--- revisao/exercicios_objeto/analise_objetos.js
+++ revisao/exercicios_objeto/analise_objetos.js
@@ -1,15 +1,32 @@
 //Dado o objeto, analise a forma que foi declarada. 
 //Em seguida, ajuste o objeto produtos da forma correta de se declarar.
-let produtos = {
-  videos: {
-    comedia: ["comedia1""comedia2"];
-    romance ["romance1", "romance2"]
-  },
-  revistas: [
-    moda = ["lalala", "lililili"],
-    saude = ["lalala", "lililili"],
-  ]
-  jogos {
-    rpg ["rpg1", "rpg2", rpg3],
-    acao: ["acao1", "God of War"]
+// let produtos = {
+//   videos: {
+//     comedia: ["comedia1""comedia2"];
+//     romance ["romance1", "romance2"]
+//   },
+//   revistas: [
+//     moda = ["lalala", "lililili"],
+//     saude = ["lalala", "lililili"],
+//   ]
+//   jogos {
+//     rpg ["rpg1", "rpg2", rpg3],
+//     acao: ["acao1", "God of War"]
+//   }
+
+  //****MINHA RESOLUÇÃO******/
+  let produtos = {
+    videos: {
+      comedia: ["comedia1","comedia2"],
+      romance: ["romance1", "romance2"]
+    },
+    revistas:{
+      moda: ["lalala", "lililili"],
+      saude: ["lalala", "lililili"]
+    },
+    jogos: {
+      rpg: ["rpg1", "rpg2", "rpg3"],
+      acao: ["acao1", "God of War"]
+    },
   }
+  console.log(produtos)
