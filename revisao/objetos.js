ad00bbff48dac2a5a1598c3d8f38f48f5e582d5c
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-06-02 09:23:11 -0300

    Resolvendo exercício de objeto

--- revisao/objetos.js
+++ revisao/objetos.js
@@ -1,19 +1,67 @@
-<<<<<<< HEAD
-const 
-=======
-const pessoa = {
-  personalidade: ['personalide1', 'personalide2'],
-  areaAtuacao: 'Biologa'
-}
+// const pessoa = {
+//   personalidade: ['personalide1', 'personalide2'],
+//   areaAtuacao: 'Biologa'
+// }
 
-pessoa.personalidade[0];
+// pessoa.personalidade[0];
 
-console.log("Personalidade1: ", pessoa.personalidade[0]);
+// console.log("Personalidade1: ", pessoa.personalidade[0]);
 
 
 //exercício
 
+//1) Criar objeto clientes
+// 2) Criar um objeto para cada cliente com as seguintes propriedades:
+// CLIENTE1: 
+
+	// Nome: Dora Barreto 
+	// CPF: 223.001.221-29
+	// Valor da compra: R$ 2.000,00
+	// Modelo do celular: XXX-12
+	// Ano do celular: 2020
+	// Cor do celular: Amarelo
+	// Loja: RJ-BR
+	// Atendente: Joana Maria 
 
 
+	// CLIENTE 2: 
 
->>>>>>> 80e2d95b1ebedc34f39b90643231d5487f59be16
+	// Nome: Marcela Janino Silva 
+	// CPF: 321.101.231-29
+	// Valor da compra: R$ 1.300,00
+	// Modelo do celular: XXM-y
+	// Ano do celular: 2019
+	// Cor do celular: ROXO
+	// Loja: SP-BR
+	// Atendente: Drielly Gomes Almeida
+
+
+	// CLIENTE 3: 
+
+	// Nome: Raquel Borges Figueredo
+	// CPF: 910.311.425-01
+	// Valor da compra: R$ 4.000,00
+	// Modelo do celular: MDM-PRO
+	// Ano do celular: 2020
+	// Cor do celular: AZUL
+	// Loja: MG-BR
+    // Atendente: Fran Carneiro de Pinha
+    
+
+const objetoClientes = {
+    
+    cliente1 = {
+        nome: "Dora Barreto", 
+        cpf: "223.001.221-29",
+        valorCompra: 2.000,
+        modeloCel: "XXX-12",
+        anoCel: 2020,
+        CorCel: "Amarelo",
+        loja: "RJ-BR",
+        atendente: "Joana Maria"
+        },
+    }
+    
+ 
+    console.log(objetoClientes)
+
