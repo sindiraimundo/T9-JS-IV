ccb709a7dede1be96d71c968c5df31b5e3438c74
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-06-04 13:44:55 -0300

    resolvendo conflitos

--- orientacao_objetos/index.js
+++ orientacao_objetos/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Cliente{
     constructor(nome, email, cpf){//metodo construtor.
         this.nome = nome;// referenciando as propriedades da classe
@@ -9,16 +8,4 @@ class Cliente{
 
 //***objetivando***
 const cliente1 = new Cliente('Jessica', 'jessica@email.com', '000.000.000-00')//pegando as propriedades do construtor e tornar em um Objeto
-=======
-class Cliente {
-  constructor(nome, email, cpf) {
-    this.nome = nome;
-    this.email = email;
-    this.cpf = cpf;
-  }
-}
-
-const cliente1 = new Cliente('Jéssica', 'jessica@email.com', 'XXX.XXX.XX-XX')
-
->>>>>>> 5c507c88fa58e5964e98b4224cd35cce73c1f557
 console.log("Dados da cliente: ", cliente1);
